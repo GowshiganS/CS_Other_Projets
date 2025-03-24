a0b89ec38dfd82a0858e4e981015687323aa5375
Gowshigan <120591583+GowshiganS@users.noreply.github.com>
2025-03-24 15:52:39 +0100

    Update next.config.js

--- poker-app/next.config.js
+++ poker-app/next.config.js
@@ -2,21 +2,17 @@
 const nextConfig = {
   reactStrictMode: true,
   eslint: {
-    // Désactiver ESLint pendant la construction
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // Désactiver la vérification TypeScript pendant la construction
     ignoreBuildErrors: true,
   },
   output: 'export',
-  // Désactiver l'image optimization car elle n'est pas compatible avec le mode export statique
   images: {
     unoptimized: true,
   },
-  // Désactiver les routes API en mode statique
   experimental: {
-    appDir: true,
+    // Supprimez la ligne appDir: true
   },
 };
 
