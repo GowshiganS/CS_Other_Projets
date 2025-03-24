bbb3a7572473d8a1d237dc3768db5dfa2e885135
Gowshigan <120591583+GowshiganS@users.noreply.github.com>
2025-03-24 15:51:51 +0100

    Create layout.tsx

---
+++ poker-app/src/app/layout.tsx
@@ -0,0 +1,16 @@
+import { ReactNode } from 'react';
+import './globals.css';
+
+export default function RootLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  return (
+    <html lang="fr">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
